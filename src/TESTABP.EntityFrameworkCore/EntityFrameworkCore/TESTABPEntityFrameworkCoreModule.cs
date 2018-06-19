92fb02f64bd39f42d2e651fd08fe5d074c622cb3
Frank <1324983062@qq.com>
2018-06-19 16:02:20 +0800

    no message

---
+++ src/TESTABP.EntityFrameworkCore/EntityFrameworkCore/TESTABPEntityFrameworkCoreModule.cs
@@ -0,0 +1,17 @@
+﻿using Abp.EntityFrameworkCore;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+
+namespace TESTABP.EntityFrameworkCore
+{
+    [DependsOn(
+        typeof(TESTABPCoreModule), 
+        typeof(AbpEntityFrameworkCoreModule))]
+    public class TESTABPEntityFrameworkCoreModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(TESTABPEntityFrameworkCoreModule).GetAssembly());
+        }
+    }
+}
